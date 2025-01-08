ee8f0943c587a5e8f02a099c0fec3fdb5d22dfb9
sganeshwani <ganeshwani2003@gmail.com>
2025-01-08 13:40:41 +0530

    Day1 work done

---
+++ PerimeterOfRect.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class PerimeterOfRect {
+public static void main (String[] args) {
+Scanner sc=new Scanner(System.in);
+int length = sc.nextInt();
+int width = sc.nextInt();
+System.out.println("the perimeter of rectangle interest is: "+2*(length+width));
+}
+}
